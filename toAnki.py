3bd9af3c86d5cc0c699f54546caee899493ad0d5
glowzz <24627181@qq.com>
2019-05-14 21:18:57 +0800

    add copyfile and writeAnki_txt

--- toAnki.py
+++ toAnki.py
@@ -32,34 +32,45 @@ def formAnkiData(fromData,ankiData):
         An_picName= Enum[2]
         
        
-        str1="\"<img src=\"%s\" />" % (yearE+Qu_picName)
-        str2="\"<img src=\"%s\" />\"" % (yearE+An_picName)
+        str1="\"<img src=\"\"%s\"\" />\"" % (yearE+Qu_picName)
+        str2="\"<img src=\"\"%s\"\" />\"" % (yearE+An_picName)
         ankiData.append([str1,yearE,str2])
     
     
 
 
 
-def copyfile(from_path,fileName,to_path,asName):
+def copyfile(from_path,to_path,filedata,index=1):
     if not os.path.exists(to_path):
         os.mkdir(to_path)
 #print(os.path.join(from_path,filename1))
-    shutil.copy(os.path.join(from_path,fileName),os.path.join(to_path,asName))
+    for eachfile in filedata:
+        shutil.copy(os.path.join(from_path,eachfile[0],eachfile[index]),os.path.join(to_path,eachfile[0]+eachfile[index]))
+    #shutil.copy(os.path.join(from_path,fileName),os.path.join(to_path,asName))
     
-    
+def writeAnki_txt(to_filepath,data2write):
+    with open(to_filepath, 'w') as f:
+        for a in data2write:
+            f.write(a[0]+"\t"+a[1]+"\t"+a[2]+"\t\n")#for improve
+   
     
 def main():
     ankiData=[]
     ExcData=[]
-    from_path="C:\\Users\\Administrator\\Documents\\Snagit\\2004S"
-    to_path="D:\\media"
-    Exc_path="E:\\新建文件夹\\错题to_anki\\错题20190512.xlsm"
-    getExcData(Exc_path,ExcData)
-    formAnkiData(ExcData,ankiData)
-    print(ExcData)
-    print(ankiData)
-    frame = pd.DataFrame(ankiData)
-    frame.to_csv(os.path.join(to_path,"out.txt"))
+    from_path="C:\\Users\\Administrator\\Documents\\Snagit"
+    #to_path="D:\\media"
+    Exc_path="E:\\新建文件夹\\错题to_anki"
+    Exc_filename="错题20190512.xlsm"
+    getExcData(os.path.join(Exc_path,Exc_filename),ExcData)
+    
+    formAnkiData(ExcData,ankiData)  
+    writeAnki_txt(os.path.join(Exc_path,"out.txt"),ankiData)
+    copyfile(from_path,os.path.join(Exc_path,"media"),ExcData,1)
+    
+    #print(ExcData)
+    #print(ankiData)
+ #   frame = pd.DataFrame(ankiData)
+ #   frame.to_csv(os.path.join(to_path,"out.txt"))
     
 main()
         
