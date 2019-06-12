4e8b611431c1b6d4c22f38f75cd42d1df0b7af71
glowzz <24627181@qq.com>
2019-06-12 20:48:30 +0800

    add write2word and copy_all_file

--- toAnki.py
+++ toAnki.py
@@ -8,6 +8,9 @@ Created on Sun May 12 11:40:55 2019
 import pandas as pd
 import os
 import shutil
+from docx import Document
+from docx.shared import Inches
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 def getExcData(Exc_path,ExcData):
     df1 = pd.read_excel(Exc_path)
@@ -58,22 +61,65 @@ def writeAnki_txt(to_filepath,data2write):
     with open(to_filepath, 'w') as f:
         for a in data2write:
             f.write(a[0]+"\t"+a[1]+"\t"+a[2]+"\t\n")#for improve
+
+
+def writeDoc(Save_filepath,from_path,filedata):
+    document = Document()                #以默认模板建立文档对象
+
+    document.add_paragraph()
+    p_detail = document.add_paragraph()
+    r_detail = p_detail.add_run(u'真题错题集')
+    r_detail.font.bold = True
+    p_detail.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    
+    from_path1=os.path.join(from_path,'QU')
+    for eachfile in filedata:
+        document.add_paragraph(eachfile[0]+eachfile[1].split('.')[0])
+        document.add_picture(os.path.join(from_path1,eachfile[0],eachfile[1]),width=Inches(6))
+        document.add_paragraph()
+        document.add_paragraph()
+        document.add_paragraph()
+        
+
+    p_detail = document.add_paragraph()
+    r_detail = p_detail.add_run(u'答案')
+    r_detail.font.bold = True
+    p_detail.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    
+    from_path2=os.path.join(from_path,'AN')
+    for eachfile in filedata:
+        document.add_paragraph(eachfile[0]+eachfile[2].split('.')[0])
+        document.add_picture(os.path.join(from_path2,eachfile[0],eachfile[1]),width=Inches(4))
    
+    document.save(Save_filepath)  
+
+def copy_all_file(from_path,to_path,filedata):
+    #lists = os.listdir(from_path)
+    for eachfile in filedata:
+        #print(i)
+        shutil.copy(os.path.join(from_path,eachfile[0]+eachfile[1]),to_path)
+        shutil.copy(os.path.join(from_path,eachfile[0]+eachfile[2]),to_path)  
     
 def main():
     ankiData=[]
     ExcData=[]
     from_path="C:\\Users\\Administrator\\Documents\\Snagit"
-    #to_path="D:\\media"
+    to_path=r"C:\Users\Administrator\AppData\Roaming\Anki2\000\collection.media"
     Exc_path="E:\\新建文件夹\\错题to_anki"
     Exc_filename="错题集WWX.xls"
     getExcData(os.path.join(Exc_path,Exc_filename),ExcData)
     
     formAnkiData(ExcData,ankiData)  
+    print(ExcData)
+
+    
     writeAnki_txt(os.path.join(Exc_path,"out.txt"),ankiData)
     copyfile(from_path,os.path.join(Exc_path,"media"),ExcData,1)
     copyfile(from_path,os.path.join(Exc_path,"media"),ExcData,2)
-   # print(ExcData)
+#    writeDoc(os.path.join(Exc_path,"错题.docx"),from_path,ExcData)
+ 
+    copy_all_file(os.path.join(Exc_path,"media"),to_path,ExcData)    
+    
     #print(ankiData)
  #   frame = pd.DataFrame(ankiData)
  #   frame.to_csv(os.path.join(to_path,"out.txt"))
